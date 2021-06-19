e7fce9b8e3efde631c7aa20d3b8c3699060cf6c6
juanmojica17 <juanmojicaayure@gmail.com>
2021-06-18 21:31:40 -0400

    profe aqui almenos  ya guarda y borra otra vez y edita si se guarda un dato nuevo

---
+++ main.js
@@ -0,0 +1,128 @@
+
+let usersList = [];
+
+let updateFlag = false;
+let updateIndex = null;
+
+let userListUI = document.getElementById("userList");
+
+const userForm = document.getElementById("addUser");
+const localuserslist = usersList;
+const userStorage =()=>{
+    if (typeof Storage !== "undefined"){
+        localStorage.setItem("userStorage", JSON.stringify(usersList));
+    }
+
+        else{
+
+            alert("tu navegador no posee almacenamiento");
+    }
+}
+const renderList = () => {
+    userListUI.innerHTML = "";
+   // userListArray = usersList;
+    userListArray = JSON.parse(localStorage.getItem("userStorage"))
+    if(userListArray === null){
+        userListArray =[]
+    }else{
+    userListArray.forEach((user, index) => {
+
+        const userItemDiv = document.createElement("div");
+        userItemDiv.setAttribute("class", "userItem");
+        userListUI.appendChild(userItemDiv);
+
+
+        const userInfoDiv = document.createElement("div");
+        userInfoDiv.setAttribute("class", "userInfo");
+        userItemDiv.appendChild(userInfoDiv);
+
+
+        const marcaUserDiv = document.createElement("h4");
+        const colUserDiv = document.createElement("h4");
+        const añoUserDiv = document.createElement("h4");
+        marcaUserDiv.innerText = `${user.marca} ${user.modelo}`;
+        colUserDiv.innerText = `${user.col} `;
+        añoUserDiv.innerText = `${user.año} `;
+
+        userInfoDiv.appendChild(marcaUserDiv);
+        userInfoDiv.appendChild(colUserDiv);
+        userInfoDiv.appendChild(añoUserDiv);
+
+        const actionButtons = document.createElement("div");
+        actionButtons.setAttribute("class", "actions");
+        userItemDiv.appendChild(actionButtons);
+
+        const updateBtn = document.createElement("button");
+
+        updateBtn.setAttribute("class", "update");
+        updateBtn.addEventListener("click", () => updateUser(index, user));
+        updateBtn.setAttribute("id", "update");
+        updateBtn.innerText = "Editar";
+
+        const deleteBtn = document.createElement("button");
+
+        deleteBtn.setAttribute("class", "delete");
+        deleteBtn.addEventListener("click", () => deleteUser(index));
+        deleteBtn.innerHTML = "Eliminar";
+        deleteBtn.setAttribute("id", "delete");
+
+        actionButtons.appendChild(updateBtn);
+        actionButtons.appendChild(deleteBtn);
+    });
+}};
+
+const createUpdateUser = event => {
+    event.preventDefault();
+    if (updateFlag) {
+        let updatedUser = {
+            marca: document.getElementById("marca").value,
+            modelo: document.getElementById("modelo").value,
+            col: document.getElementById("col").value,
+            año: document.getElementById("año").value
+        };
+
+        usersList[updateIndex] = updatedUser;
+
+        updateFlag = false;
+        updateIndex = null;
+        renderList();
+    } else {
+        let user = {
+            marca: document.getElementById("marca").value,
+            modelo: document.getElementById("modelo").value,
+            col: document.getElementById("col").value,
+            año: document.getElementById("año").value
+        };
+
+        usersList.push(...localuserslist, user);
+        userStorage();
+        renderList();
+    }
+    userForm.reset();
+};
+
+const updateUser= (index, user) => {
+    console.log(index);
+    console.log(user);
+    document.getElementById("marca").value = user.marca;
+    document.getElementById("modelo").value = user.modelo;
+    document.getElementById("col").value = user.col;
+    document.getElementById("año").value = user.año;
+
+    updateFlag = true;
+    updateIndex = index;
+    userStorage();
+    renderList();
+};
+
+const deleteUser = index => {
+    usersList.splice(index, 1);
+    let deleteuserstorage = JSON.parse(localStorage.getItem("userStorage"));
+    usersList = deleteuserstorage;
+    usersList.splice(index, 1);
+    userStorage();
+    renderList();
+};
+
+userForm.addEventListener("submit", createUpdateUser);
+document.addEventListener("DOMContentLoaded", renderList);
